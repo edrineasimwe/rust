7de505eeff995632911fa1da0a1a6eed8eebffae
edrineasimwe <edrineasiimwe12@gmail.com>
2025-03-18 15:39:47 +0300

    imp the struct

--- Structs/src/main.rs
+++ Structs/src/main.rs
@@ -4,8 +4,16 @@ fn main() {
         length: 10.0
     };
     let react_area = area(rectangle1.width, rectangle1.length);
+    let react_parameter   = parameter(rectangle1.width, rectangle1.length);
+
 
     println!("the area of a rectangle is {:#?}", react_area);
+    println!("the parameter of a rectangle is {:#?}", react_parameter);
+
+    println!("the area of a rectangle is {:#?}", rectangle1.area());
+    println!("the parameter of a rectangle is {:#?}", rectangle1.parameter());
+
+
 }
 
 #[derive(Debug)]
@@ -21,6 +29,15 @@ fn area(width:f64, length:f64) -> f64{
 fn parameter(length:f64, width:f64) -> f64{
     2.0 * (width + length)
 }
+
+
 impl Rectangle {
-    
+    fn area(&self) -> f64{
+        self.width*self.length
+
+    }
+    fn parameter(&self) -> f64{
+        2.0 * (self.width + self.length)
+
+    }
 }
